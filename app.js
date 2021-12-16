2a5e6dfa8f2bf4a7513da2aad96e24a1934f6fbf
Tania Karageorgi <55257218+Tania1978@users.noreply.github.com>
2021-12-16 08:43:31 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+
+
+
+
+
+document.getElementById('btn').addEventListener('click', createListElement)
+var list = document.getElementById('ul')
+let todolist = []
+function createListElement() {
+    var enteredText = document.getElementById('todoinput').value
+    if(enteredText === ''){
+        alert('Please enter a todo')
+    } else {
+    if (todolist.includes(enteredText)) {
+        if (confirm('Todo already exists, you still want to add it?')) {
+            addTodo(enteredText)
+        }
+        // } else {
+        // console.log('Nothing');
+        // }
+    } else {
+        addTodo(enteredText)
+        todolist.push(enteredText)
+    }
+}
+
+}
+
+function removeItem() {
+    this.parentNode.remove();
+}
+
+function addTodo(value) {
+    var li = document.createElement("li");
+    li.innerHTML = value
+    list.appendChild(li)
+    var deleteButton = document.createElement("button");
+    deleteButton.setAttribute("id", "delete");
+    deleteButton.appendChild(document.createTextNode("Delete"));
+    ul > li.appendChild(deleteButton).addEventListener("click", removeItem);
+}
+
